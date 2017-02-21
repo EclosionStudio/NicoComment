58a4333c097f06af563a258cb56160e0159b6ddb
liu599 <970228409@qq.com>
2017-02-22 01:51:19 +0800

    clear folder

--- redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import CommentBoxReducer from './CommentBoxReducer';
-
-const rootReducer = combineReducers({
-    userComment: CommentBoxReducer,
-})
-
-export default rootReducer;
